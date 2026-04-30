9575cc2332e39639a7e3ced86286575e3001eb30
Guilherme Rocha <guilhermerochabd@gmail.com>
2026-04-30 00:58:19 -0300

    praticando atividades vetor

--- assets/js/logica-04-metodos-string.js
+++ assets/js/logica-04-metodos-string.js
@@ -1,168 +1,168 @@
-function exemploToLowerCase() {
-    let nome = "John Doe";
-    let nomeMinusculo = nome.toLowerCase();
-    console.log("Nome minúsculo:", nomeMinusculo);
-}
-
-function exemploToUpperCase() {
-    let nome = "John Doe";
-    let nomeMaiusculo = nome.toUpperCase();
-
-    console.log("Nome maiúsculo:", nomeMaiusculo);
-}
-
-function exemploTamanho() {
-    let nome = "John Doe";
-    let quantidadeCaracteres = nome.length;
-
-    console.log("Texto; '" + nome + "'");
-    console.log("Quantidade de caracteres:", quantidadeCaracteres);
-}
-
-function exemploSubstituicao() {
-    let texto = "Abacate, Uva, Maca, Pera";
-    let textoCorrigido = texto.replace("Maca", "Maçã");
-
-    console.log("Texto:", texto);
-    console.log("Texto corrigido:", textoCorrigido);
-}
-
-function exemploSubstituicaoNapropriaVariavel() {
-    let texto = prompt("Digite um preço"); // => "450.39"
-    // "R$ 450,39" => "R$ 450.39"
-    texto = texto.replace(",", ".");
-    texto = texto.replace("R$", ""); 
-    texto = texto.replaceAll(" ", "");
-
-    console.log("Texto sanitizado:" + texto);
-}
-
-function removerEspacosDoFim() {
-    let texto = "   O batman vive anoite, de dia ele dorme   ";
-
-    console.log("Texto original: '" + texto + "'");
-
-    texto = texto.trimEnd();
-    console.log("Texto sem espaços no fim: '" + texto + "'");
-}
-
-function removerEspacosDoComeco() {
-    let texto = "  O batman vive anoite, de dia ele dorme   ";
-
-    console.log("O texto original: '" + texto + "'");
-
-    texto = texto.trimStart();
-    console.log("Texto sem espaços no começo: '" + texto + "'");
-}
-
-function removerEspacosDoComecoFinal() {
-    let texto = " O batman vive anoite, de dia ele dorme ";
-
-    console.log("O texto original: '" + texto + "'");
-    texto = texto.trim();
-    console.log("Texto sem espaços no começo e no fim: '" + texto + "'");
-}
-
-function agregarCaracteresFim() {
-    let nome = "Paulo C";
-    console.log("Nome do cartão: " + nome.padEnd(20, "*"));
-}
-
-function agregarCaracteresComeco() {
-    let finalCartaoCredito = "7292"
-    console.log("Cartão de crédito: " + finalCartaoCredito.padStart(12, "*"));
-}
-
-function verificarSeComecaCom() {
-    let texto = prompt("Digite um nome");
-
-    // Verificar se o nome começa com a letra "P"]
-    if (texto.toUpperCase().startsWith("P") === true) {
-        alert("Começa com a letra 'P' , pois tem a senha secreta");
-    } else {
-        alert("Não pode entrar na festa");
+    function exemploToLowerCase() {
+        let nome = "John Doe";
+        let nomeMinusculo = nome.toLowerCase();
+        console.log("Nome minúsculo:", nomeMinusculo);
     }
-}
 
-function verificarSeContem(){
-    let nomeEpisodio = prompt("Digite o nome do episodio");
+    function exemploToUpperCase() {
+        let nome = "John Doe";
+        let nomeMaiusculo = nome.toUpperCase();
 
-    // Verificar se é a primeira temporada "S01", exemplo "Lost S01E04"
-    if (nomeEpisodio.includes("S01") === true) {
-        alert("Primeira temporada");
-    } else if (nomeEpisodio.includes("S02") === true) {
-        alert("Segunda temporada");
-    } else {
-        alert("Ao infinito e além");
+        console.log("Nome maiúsculo:", nomeMaiusculo);
     }
-}
 
-    function verificarSeTerminaCom(){
-        //Verificar se termina com 'chocolate'
+    function exemploTamanho() {
+        let nome = "John Doe";
+        let quantidadeCaracteres = nome.length;
 
-        let bolo = prompt("Digite o nome do bolo");
-        if (bolo.endsWith("chocolate") === true) {
-            alert("Boa escolha");
+        console.log("Texto; '" + nome + "'");
+        console.log("Quantidade de caracteres:", quantidadeCaracteres);
+    }
+
+    function exemploSubstituicao() {
+        let texto = "Abacate, Uva, Maca, Pera";
+        let textoCorrigido = texto.replace("Maca", "Maçã");
+
+        console.log("Texto:", texto);
+        console.log("Texto corrigido:", textoCorrigido);
+    }
+
+    function exemploSubstituicaoNapropriaVariavel() {
+        let texto = prompt("Digite um preço"); // => "450.39"
+        // "R$ 450,39" => "R$ 450.39"
+        texto = texto.replace(",", ".");
+        texto = texto.replace("R$", ""); 
+        texto = texto.replaceAll(" ", "");
+
+        console.log("Texto sanitizado:" + texto);
+    }
+
+    function removerEspacosDoFim() {
+        let texto = "   O batman vive anoite, de dia ele dorme   ";
+
+        console.log("Texto original: '" + texto + "'");
+
+        texto = texto.trimEnd();
+        console.log("Texto sem espaços no fim: '" + texto + "'");
+    }
+
+    function removerEspacosDoComeco() {
+        let texto = "  O batman vive anoite, de dia ele dorme   ";
+
+        console.log("O texto original: '" + texto + "'");
+
+        texto = texto.trimStart();
+        console.log("Texto sem espaços no começo: '" + texto + "'");
+    }
+
+    function removerEspacosDoComecoFinal() {
+        let texto = " O batman vive anoite, de dia ele dorme ";
+
+        console.log("O texto original: '" + texto + "'");
+        texto = texto.trim();
+        console.log("Texto sem espaços no começo e no fim: '" + texto + "'");
+    }
+
+    function agregarCaracteresFim() {
+        let nome = "Paulo C";
+        console.log("Nome do cartão: " + nome.padEnd(20, "*"));
+    }
+
+    function agregarCaracteresComeco() {
+        let finalCartaoCredito = "7292"
+        console.log("Cartão de crédito: " + finalCartaoCredito.padStart(12, "*"));
+    }
+
+    function verificarSeComecaCom() {
+        let texto = prompt("Digite um nome");
+
+        // Verificar se o nome começa com a letra "P"]
+        if (texto.toUpperCase().startsWith("P") === true) {
+            alert("Começa com a letra 'P' , pois tem a senha secreta");
         } else {
-            alert("Não como outro bolo");
-        }   
+            alert("Não pode entrar na festa");
+        }
     }
 
-    function pegarPosicao() {
-        let fruta = "Abacate";
-        let primeiroCaracter = fruta[0];
+    function verificarSeContem(){
+        let nomeEpisodio = prompt("Digite o nome do episodio");
 
-        console.log("Fruta: " + fruta + "\nPrimeira posição: " + primeiroCaracter);
+        // Verificar se é a primeira temporada "S01", exemplo "Lost S01E04"
+        if (nomeEpisodio.includes("S01") === true) {
+            alert("Primeira temporada");
+        } else if (nomeEpisodio.includes("S02") === true) {
+            alert("Segunda temporada");
+        } else {
+            alert("Ao infinito e além");
+        }
     }
 
-    function quebrarString() {
-        // String         01234
-        let horaMinuto = "20:30";
+        function verificarSeTerminaCom(){
+            //Verificar se termina com 'chocolate'
 
-        let partes = horaMinuto.split(":");
-        //
-        // split => ["20", "30"]
+            let bolo = prompt("Digite o nome do bolo");
+            if (bolo.endsWith("chocolate") === true) {
+                alert("Boa escolha");
+            } else {
+                alert("Não como outro bolo");
+            }   
+        }
 
-        console.log(partes);
-        let hora = partes[0];
-        let minuto = partes[1];
+        function pegarPosicao() {
+            let fruta = "Abacate";
+            let primeiroCaracter = fruta[0];
 
-        console.log("Hora: " + hora);
-        console.log("Minuto: " + minuto);
+            console.log("Fruta: " + fruta + "\nPrimeira posição: " + primeiroCaracter);
+        }
+
+        function quebrarString() {
+            // String         01234
+            let horaMinuto = "20:30";
+
+            let partes = horaMinuto.split(":");
+            //
+            // split => ["20", "30"]
+
+            console.log(partes);
+            let hora = partes[0];
+            let minuto = partes[1];
+
+            console.log("Hora: " + hora);
+            console.log("Minuto: " + minuto);
+        }
+        
+        function exemploPegarPartesDaString() {
+            let nomeCompleto = "Ruan Costa";
+            // Ruan Costa
+            // R => 0(Começa nesta posição)
+            // u => 1
+            // a => 2
+            // n => 3
+            //   => 4 (Para nesta posição)
+            // C => 5
+            // o => 6
+            // s => 7
+            // t => 8
+            // a => 9
+                let nome = nomeCompleto.slice(0, 4); // "Ruan"
+        let sobrenome = nomeCompleto.slice(5, 10);
     }
-    
-    function exemploPegarPartesDaString() {
-        let nomeCompleto = "Ruan Costa";
-        // Ruan Costa
-        // R => 0(Começa nesta posição)
-        // u => 1
-        // a => 2
-        // n => 3
-        //   => 4 (Para nesta posição)
-        // C => 5
-        // o => 6
-        // s => 7
-        // t => 8
-        // a => 9
-            let nome = nomeCompleto.slice(0, 4); // "Ruan"
-    let sobrenome = nomeCompleto.slice(5, 10);
-}
 
-function pegarIndiceDoCaracter(){
-    let texto = "Abacate Uva Pera";
-    let ultimoIndiceDoEspaco = texto.lastIndexOf(" ");
+    function pegarIndiceDoCaracter(){
+        let texto = "Abacate Uva Pera";
+        let ultimoIndiceDoEspaco = texto.lastIndexOf(" ");
 
-    let ultimaPalavra = texto.slice(ultimoIndiceDoEspaco + 1, texto.length);
-    console.log(texto);
-    console.log(ultimaPalavra);
-}
+        let ultimaPalavra = texto.slice(ultimoIndiceDoEspaco + 1, texto.length);
+        console.log(texto);
+        console.log(ultimaPalavra);
+    }
 
-function descobrirIndiceDoCaracter(){
-    //                  0123456789....
-    let numeroCartao = "3010 2039 2019 2030";
-    let indicePrimeiroEspaco = numeroCartao.indexOf(" ");
-    let primeiraParteCartao = numeroCartao.slice(0, indicePrimeiroEspaco);
+    function descobrirIndiceDoCaracter(){
+        //                  0123456789....
+        let numeroCartao = "3010 2039 2019 2030";
+        let indicePrimeiroEspaco = numeroCartao.indexOf(" ");
+        let primeiraParteCartao = numeroCartao.slice(0, indicePrimeiroEspaco);
 
-    console.log("Cartão: " + numeroCartao);
-    console.log("Primeira parte do cartão: " + primeiraParteCartao);
-}
+        console.log("Cartão: " + numeroCartao);
+        console.log("Primeira parte do cartão: " + primeiraParteCartao);
+    }
--- assets/js/logica-exercicios-vetor.js
+++ assets/js/logica-exercicios-vetor.js
@@ -41,47 +41,124 @@ function exemplo01(){
     Apresentar todas os dados dos vetores*/
 
 
-function exercicios02(){
+function exercicios02() {
     let materias = [];
     let cargaHoraria = [];
     let valoresHoras = [];
 
-    materias.push("Geografia")[0];
-    materias.push("Matemática")[1];
-    materias.push("fisíca")[2];
+    // Adicionando dados corretamente
+    materias.push("Geografia");
+    materias.push("Matemática");
+    materias.push("Física");
 
-    cargaHoraria.push(parseInt("100")[0]);
-    cargaHoraria.push(parseInt("200")[1]);
-    cargaHoraria.push(parseInt("250")[2]);
+    cargaHoraria.push(100);
+    cargaHoraria.push(200);
+    cargaHoraria.push(250);
 
-    valoresHoras.push(parseFloat("25.3")[0]);
-    valoresHoras.push(parseFloat("35.3")[1]);
-    valoresHoras.push(parseFloat("65.3")[2]);
+    valoresHoras.push(25.3);
+    valoresHoras.push(35.3);
+    valoresHoras.push(65.3);
 
-    alert("Materias" +
-        "\nPrimeiro " + materias[0] + " " + cargaHoraria[0]+ " " + valoresHoras[0]+
-        "\nSegundo " + materias[1]+ " "+ cargaHoraria[1]+ " "+ valoresHoras[1]+
-        "\nTerceiro " + materias[2]+ " " + cargaHoraria[2]+" "+ valoresHoras[2]
-
+    // Mostrar dados
+    alert("Matérias" +
+        "\nPrimeiro: " + materias[0] + " " + cargaHoraria[0] + " " + valoresHoras[0] +
+        "\nSegundo: " + materias[1] + " " + cargaHoraria[1] + " " + valoresHoras[1] +
+        "\nTerceiro: " + materias[2] + " " + cargaHoraria[2] + " " + valoresHoras[2]
     );
-    materias[1] = "História"; 
-    cargaHoraria[0] = 150; 
 
-    alert("Depois Das ALTERAÇÕES" +
+    // Alterações
+    materias[1] = "História";
+    cargaHoraria[0] = 150;
+
+    // Mostrar depois
+    alert("Depois das alterações" +
         "\nPrimeiro: " + materias[0] + " - " + cargaHoraria[0] + " - " + valoresHoras[0] +
         "\nSegundo: " + materias[1] + " - " + cargaHoraria[1] + " - " + valoresHoras[1] +
         "\nTerceiro: " + materias[2] + " - " + cargaHoraria[2] + " - " + valoresHoras[2]
     );
 }
 
-   
+/*3. Criar uma função exercicio03
+    Criar um vetor de destinosTuristicos
+    Solicitar para o usuário o nome de 4 destinos turísticos
+    Adicionar os 4 destinos no vetor
+    Apresentar todos os destinos turísticos
+    Solicitar para o usuário um novo nome para o segundo destino
+    Alterar o segundo destino turístico
+    Solicitar para o usuário mais 1 destino turístico
+    Adicionar o novo destino no vetor
+    Apagar o primeiro destino turístico
+    Apresentar todos os destinos turísticos*/
 
+    function exercicio03(){
+        let destinosTuristicos = [];
+       
+        destinosTuristicos.push(prompt("Digite o primeiro destino Turistico"));
+        destinosTuristicos.push(prompt("Digite o segundo destino Turistico"));
+        destinosTuristicos.push(prompt("Digite o terceiro destino Turistico"));
+        destinosTuristicos.push(prompt("Digite o quarto destino Turistico"));
 
+        alert("Destino Turisticos:" +
+            "\nPrimeiro: " + destinosTuristicos[0] +
+            "\nSegundo: " + destinosTuristicos[1] +
+            "\nTerceiro: " + destinosTuristicos[2] +
+            "\nQuarto: " + destinosTuristicos[3] 
+        );
 
+        destinosTuristicos[1] = prompt("Digite um novo Nome ");
+        destinosTuristicos.push(prompt("Digite mais um ponto turistico"));
+        destinosTuristicos.splice(0 , 1);
+        alert("Destino Atualizado " +
+            "\nPrimeiro: " + destinosTuristicos[0] +
+            "\nSegundo: " + destinosTuristicos[1] +
+            "\nTerceiro: " + destinosTuristicos[2] +
+            "\nQuarto: " + destinosTuristicos[3] 
+        );
+        
 
+    }
 
+   /*4. Criar uma função exercicio04
+    Criar um vetor de restaurantes
+    Criar um vetor de cidades
+    Criar um vetor de valoresMedios
 
+    Solicitar para o usuário os dados de 3 restaurantes
+    Para cada restaurante solicitar:
+        Nome do restaurante
+        Cidade onde fica localizado
+        Valor médio gasto por pessoa
 
+    Adicionar os dados nos vetores
+    Apresentar todos os dados dos restaurantes
+    Solicitar para o usuário um novo nome para o primeiro restaurante
+    Alterar o nome do primeiro restaurante
+    Solicitar para o usuário um novo valor médio para o terceiro restaurante
+    Alterar o valor médio do terceiro restaurante
+    Apresentar todos os dados dos restaurantes*/
 
+function exercicio04(){
+    let restaurante = [];
+    let cidades = [];
+    let valoresMedios = [];
 
+    restaurante.push(prompt("Digite o primeiro Restaurante"));
+    restaurante.push(prompt("Digite o segundo Restaurante"));
+    restaurante.push(prompt("Digite o terceiro Restaurante"));
 
+    cidades.push(prompt("Digite a primeira cidade"));
+    cidades.push(prompt("Digite a segunda cidade"));
+    cidades.push(prompt("Digite a terceira cidade"));
+
+    valoresMedios.push(parseFloat(prompt("Digite o primeiro Valor")));
+    valoresMedios.push(parseFloat(prompt("Digite o segundo Valor")));
+    valoresMedios.push(parseFloat(prompt("Digite o terceiro Valor")));
+
+    alert("Restaurantes: " +
+        "\nPrimeiro: " + restaurante[0] + " - " + cidades[0] + " - " + valoresMedios[0] +
+        "\nSegundo: " + restaurante[1] + " - " + cidades[1] + " - " + valoresMedios[1] +
+        "\nTerceiro: " + restaurante[2] + " - " + cidades[2] + " - " + valoresMedios[2] 
+    );
+
+    
+}
