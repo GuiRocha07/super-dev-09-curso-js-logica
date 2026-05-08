db208d3c11d50cb2bf683a1e7201023752fcdcbb
Guilherme Rocha <guilhermerochabd@gmail.com>
2026-05-08 19:10:36 -0300

    Atividade for

--- assets/js/logica-08-vetor-com-for.js
+++ assets/js/logica-08-vetor-com-for.js
@@ -201,7 +201,6 @@ function exercicio01() {
         texto = texto + nomes[i] + "\n";
     }
 
-    // Mostrar todos juntos
     alert(texto);
 }
 
@@ -213,27 +212,77 @@ function exercicio01() {
 
 function exercicio02() {
 
-    // Criando vetor vazio
+
     let idades = [];
 
-    // Solicitar 4 idades
     for (let i = 0; i < 4; i += 1) {
 
         let idade = parseInt(prompt("Digite a idade"));
-
-        // Adicionando no vetor
         idades.push(idade);
     }
 
-    // Variável para guardar todas as idades
     let texto = "";
 
-    // Percorrer vetor
     for (let i = 0; i < idades.length; i += 1) {
 
         texto = texto + idades[i] + "\n";
     }
 
-    // Mostrar todas juntas
     alert(texto);
 }
+/*Criar uma função exercicio03
+   Criar um vetor de cidades
+   Solicitar para o usuário 5 cidades
+   Adicionar as 5 cidades no vetor
+   Utilizar um for para apresentar todas as cidades*/
+function exercicio03(){
+
+    // Criar vetor
+    let cidades = [];
+
+    // Solicitar 5 cidades
+    for(let i = 0; i < 5; i += 1){
+
+        let cidade = prompt("Digite uma cidade");
+
+        cidades.push(cidade);
+    }
+
+    // Variável para juntar o texto
+    let texto = "";
+
+    // Percorrer vetor
+    for(let i = 0; i < cidades.length; i += 1){
+
+        texto = texto + cidades[i] + "\n";
+    }
+
+    // Mostrar tudo junto
+    alert(texto);
+}
+
+/*Criar uma função exercicio04
+   Criar um vetor de números
+   Solicitar para o usuário 5 números
+   Adicionar os 5 números no vetor
+   Utilizar um for para somar todos os números do vetor
+   Ao final, mostrar a soma total*/
+
+function exercicio04(){
+    let numeros = [];
+    
+for(let i = 0; i < 5; i =+ 1);{
+    let numero = parseInt(prompt("Digite um numero"));
+    numeros.push(numero);
+        
+}
+    let soma = 0;
+
+for(let i = 0; i < numeros.length; i += 1){
+
+    soma = soma + numeros[i];
+
+}
+   alert("Soma total : " + soma);
+}
+
